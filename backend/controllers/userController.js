f176f994be0c3d0a54d7e48756c9a1f973efc3b2
raghav <raghavattri13@gmail.com>
2024-02-20 19:58:15 +0530

    updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,18 +2,18 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-// User registration
+
 exports.register = async (req, res) => {
   try {
     const { username,email,password,role} = req.body;
 
-    // Check if user already exists
+    
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    // Create a new user
+ 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ username, password: hashedPassword,role,email });
     await newUser.save();
@@ -24,7 +24,6 @@ exports.register = async (req, res) => {
   }
 };
 
-// User login
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -35,13 +34,12 @@ exports.login = async (req, res) => {
       return res.status(401).json({ message: 'Invalid username or password' });
     }
 
-    // Validate password
+ 
     const isValidPassword = await bcrypt.compare(password, user.password);
     if (!isValidPassword) {
       return res.status(401).json({ message: 'Invalid username or password' });
     }
 
-    // Generate JWT token
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
     res.json({ token });
